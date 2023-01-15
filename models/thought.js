00e8f15c8b1791f88eda27de04b45da0e46bc1d7
Melisa Z <melzahirovic@gmail.com>
2023-01-15 17:57:52 -0600

    models added

---
+++ models/thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const moment = require('moment')
+
+const thoughtSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
